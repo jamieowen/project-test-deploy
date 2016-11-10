809907037f4094b8404c7b929ee68651fd4347b3
jamieowen <hello@jamieowen.com>
2016-11-10 09:14:21 +0000

    added deploy stuff

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 var deploy = require('gh-pages');
 
-deploy.publish( './deploy', { branch: 'deplpy' }, function(){
+deploy.publish( './deploy', { branch: 'deploy' }, function(){
     console.log( 'deployed..' );
 });
